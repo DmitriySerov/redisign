85df64f02baa49ca84e890ca13f4240ee77c2166
Dmitry <dima_serov2002@mail.ru>
2020-03-24 21:49:10 +0300

    feat:add projexts section and slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // modal-window
     var modal = $('.modal'),
         modalBtn = $('[data-toggle="modle"]'),
         closeBtn = $('.modal__close');
@@ -14,18 +15,38 @@ $(document).ready(function () {
             modal.removeClass('modal--visible');
         };
     });
+
+    // scroll-up
     $(window).scroll(function(){
 		if($(window).scrollTop()>500){
 			$('#scroll_top').fadeIn(900)
 		}else{
 			$('#scroll_top').fadeOut(700)
 		}
-	});
+    });
     $(function(){
         $('#scroll_top').click(function(){
             $('html, body').animate({scrollTop: 0}, 600);
             return false;
         });
     });
+    // slider-projects
+    var mySwiper = new Swiper ('.swiper-container', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+          },
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+      })
 
+      var next = $('.swiper-button-next');
+      var prev = $('.swiper-button-prev');
+      var bullets = $('.swiper-pagination');
+
+      next.css('left', prev.width() + 10 + bullets.width() + 10)
+      bullets.css('left', prev.width() + 10)
 });

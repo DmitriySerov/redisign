c3e42402210c46ff231469464255da980b0bc5fa
Dmitry <dima_serov2002@mail.ru>
2020-03-25 14:20:42 +0300

    feat: slider

--- js/main.js
+++ js/main.js
@@ -49,4 +49,5 @@ $(document).ready(function () {
 
       next.css('left', prev.width() + 10 + bullets.width() + 10)
       bullets.css('left', prev.width() + 10)
+    //   slider steps section
 });

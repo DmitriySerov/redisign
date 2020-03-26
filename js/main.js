66230338a183c76e74916d7a477515297ab4c603
Dmitry <dima_serov2002@mail.ru>
2020-03-26 15:57:56 +0300

    feat: add validation form

--- js/main.js
+++ js/main.js
@@ -52,4 +52,97 @@ $(document).ready(function () {
     //   slider steps section
 
     new WOW().init();
+
+    // Валидация форм
+    $('.modal__form').validate({
+        errorClass: "invalid",
+        errorElement: "div",
+        rules: {
+            // строчное правило
+            userName: {
+                required: true,
+                minlength: 2,
+                maxlength: 15
+            },
+            userPhone: "required",
+            // Правило-объект
+            userEmail: {
+              required: true,
+              email: true
+            }
+          },
+        //   сообщения
+          messages: {
+            userName: {
+                required: "Поле обязательно",
+                minlength: "Имя не короче 2 букв",
+                maxlength: "Имя не длиннее 15 букв"
+            },
+            userPhone: "Поле обязательно",
+            userEmail: {
+              required: "Поле обязательно",
+              email: "Введите корректный email"
+            }
+          }
+    });
+    // Валидация формы контроля
+    $('.control__form').validate({
+        errorClass: "invalid",
+        errorElement: "div",
+        rules: {
+            // строчное правило
+            userName: {
+                required: true,
+                minlength: 2,
+                maxlength: 15
+            },
+            userPhone: "required"
+          },
+        //   сообщения
+          messages: {
+            userName: {
+                required: "Поле обязательно",
+                minlength: "Имя не короче 2 букв",
+                maxlength: "Имя не длиннее 15 букв"
+            },
+            userPhone: "Поле обязательно",
+          }
+    });
+    // валидация формы footer
+
+    $('.footer__form').validate({
+        errorClass: "invalid",
+        errorElement: "div",
+        rules: {
+            // строчное правило
+            userName: {
+                required: true,
+                minlength: 2,
+                maxlength: 15
+            },
+            userPhone: "required",
+            userQuestion: {
+                required: true,
+                minlength: 1,
+                maxlength: 150
+            }
+          },
+        //   сообщения
+          messages: {
+            userName: {
+                required: "Поле обязательно",
+                minlength: "Имя не короче 2 букв",
+                maxlength: "Имя не длиннее 15 букв"
+            },
+            userPhone: "Поле обязательно",
+            userQuestion: {
+                required: "Поле обязательно",
+                minlength: "Введите вопрос",
+                maxlength: "Максмальное кол-во символов 50"
+            }
+          }
+    });
+
+    // Маска для телефона
+    $('[type=tel]').mask('+7(000) 000-00-00', {placeholder: "+7(___)___-__-__"});
 });
